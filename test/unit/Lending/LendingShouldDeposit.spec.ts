53ba3d32d80f3e6c6e4a5dcb2133c0d12bff2d8e
Alex Johnson <ajohnson2389@gmail.com>
2022-04-27 15:20:14 -0400

    Unit testing

--- test/unit/Lending/LendingShouldDeposit.spec.ts
+++ test/unit/Lending/LendingShouldDeposit.spec.ts
@@ -1,8 +1,75 @@
+import { ethers } from "hardhat";
+import { expect, assert } from "chai";
+import { BigNumber } from "ethers";
+import { parseEther } from "ethers/lib/utils";
+
 export const shouldDeposit = (): void => {
   //   // to silent warning for duplicate definition of Transfer event
   //   ethers.utils.Logger.setLogLevel(ethers.utils.Logger.levels.OFF);
 
   context(`#deposit`, async function () {
-    it(`should start here...`, async function () {});
+    it(`should revert if the token amount is not greater than 0`, async function () {
+      const amount = ethers.constants.Zero;
+
+      await expect(
+        this.lending
+          .connect(this.signers.alice)
+          .deposit(this.mocks.mockUsdc.address, amount)
+      ).to.be.revertedWith(`NeedsMoreThanZero`);
+    });
+
+    it(`should emit proper event`, async function () {
+      const amount: BigNumber = ethers.constants.One;
+
+      await expect(
+        this.lending
+          .connect(this.signers.alice)
+          .deposit(this.mocks.mockUsdc.address, amount)
+      )
+        .to.emit(this.lending, `Deposit`)
+        .withArgs(
+          this.signers.alice.address,
+          this.mocks.mockUsdc.address,
+          amount
+        );
+    });
+
+    it(`should update storage variable properly`, async function () {
+      const previousAccountToTokenDeposits: BigNumber =
+        await this.lending.s_accountToTokenDeposits(
+          this.signers.alice.address,
+          this.mocks.mockUsdc.address
+        );
+
+      const amount: BigNumber = parseEther(`1`);
+
+      await this.lending
+        .connect(this.signers.alice)
+        .deposit(this.mocks.mockUsdc.address, amount);
+
+      const currentAccountToTokenDeposits: BigNumber =
+        await this.lending.s_accountToTokenDeposits(
+          this.signers.alice.address,
+          this.mocks.mockUsdc.address
+        );
+
+      assert(
+        currentAccountToTokenDeposits.toBigInt() ===
+          previousAccountToTokenDeposits.add(amount).toBigInt(),
+        `New value should equal previous plus amount`
+      );
+    });
+
+    it(`should revert with TransferFailed error`, async function () {
+      await this.mocks.mockUsdc.mock.transferFrom.returns(false);
+
+      const amount: BigNumber = parseEther(`1`);
+
+      await expect(
+        this.lending
+          .connect(this.signers.alice)
+          .deposit(this.mocks.mockUsdc.address, amount)
+      ).to.be.revertedWith(`TransferFailed`);
+    });
   });
 };
